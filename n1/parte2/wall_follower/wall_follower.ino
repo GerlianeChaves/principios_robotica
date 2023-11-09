10d57dd3a3e4434b430057a8ea32287a8ac125c6
Guilherme Abreu Carvalho <guilhermeabreucarvalho2000@gmail.com>
2023-11-09 09:44:15 -0300

    Update wall_follower.ino

--- n1/parte2/wall_follower/wall_follower.ino
+++ n1/parte2/wall_follower/wall_follower.ino
@@ -13,56 +13,59 @@
 #include <Ultrasonic.h>
 
 //sensor ultrassonico
-#define tigger A0
-#define echo A1
+#define echo1 A0
+#define trigger1 A1
+#define echo2 A2
+#define trigger2 A3
 
-Ultrasonic ultrasonic (tigger, echo);
+Ultrasonic ultrasonic1 (trigger1, echo1);
+Ultrasonic ultrasonic2 (trigger2, echo2);
 //motores
-AF_DCMotor M3(3);
+AF_DCMotor M1(1);
 AF_DCMotor M4(4);
 
 void setup(){
   Serial.begin(9600);
-  Serial.println("Lendo os dados do sensor...");
+  M1.setSpeed(255);
+  M4.setSpeed(255);
 }
 
 void seguir(){
-  M3.run(FORWARD);
-  M4.run(FORWARD);
+  M1.run(BACKWARD);
+  M4.run(BACKWARD);
   Serial.println("seguindo a parede");
 }
 
 void desvio(){
   Serial.println("Desviando");
-  M3.run(BACKWARD); //girar o motor que está ao lado do sensor
+  //M1.setSpeed(150);
+  M1.run(FORWARD); //girar o motor que está ao lado do sensor
   M4.run(RELEASE);
 }
-void ultrasonicSensor(){
-  //  Lê as infos do sensor
-  float distancia = ultrasonic.read();
-  if (distancia <30){
-    //  apresenta as infos no monitor
-    Serial.println("Distância em cm: ");
-    Serial.println(distancia);
-    delay(1000);
-  }
+
+void procurar_parede() {
+  Serial.println("Me perdi, estou procurando uma parede");
+  M4.run(RELEASE); //girar o motor que está ao lado do sensor
+  M1.run(FORWARD);
 }
 
 void loop(){
-  float distancia = ultrasonic.read();
-  if (distancia >20 && distancia <40){
+  float dist_sensor1 = ultrasonic1.read();
+  float dist_sensor2 = ultrasonic2.read();
+  if (dist_sensor1 > 20 && dist_sensor1 < 40) {
     Serial.println("Distância em cm: ");
-    Serial.println(distancia);
+    //Serial.println(dist_sensor1);
     seguir();  
-  }if (distancia <20){
+  } if (dist_sensor1 <= 20) {
     Serial.println("Distância em cm: ");
-    Serial.println(distancia);
+    //Serial.println(dist_sensor1);
     desvio();
-  }else{
-    Serial.println("Me perdi");
+  }// else{
+  if (dist_sensor1 >= 40) {
+    procurar_parede();
   }
   
-
+  Serial.println(dist_sensor1);
 //  motor.setSpeed(200);
 //  M3.run(FORWARD);
 //  motor3.run(FORWARD);
