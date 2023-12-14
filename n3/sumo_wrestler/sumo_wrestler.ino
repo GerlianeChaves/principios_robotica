212fecf5661639d620c050d9b5ea84507bac3d92
Guilherme Abreu Carvalho <guilhermeabreucarvalho2000@gmail.com>
2023-12-14 12:22:50 -0300

    Update sumo_wrestler.ino

--- n3/sumo_wrestler/sumo_wrestler.ino
+++ n3/sumo_wrestler/sumo_wrestler.ino
@@ -1,6 +1,6 @@
 // Sumo Wrestler Robot
-// Disciplina: Principios de Robótica      Prof.: Daniel Duarte
-// Alunos: Gerliane Chaves e Guilherme Abreu
+// Discipline: Principles of Robotics      Prof.: Daniel Duarte
+// Students: Gerliane Chaves e Guilherme Abreu
 
 // libraries
 #include <AFMotor.h>
