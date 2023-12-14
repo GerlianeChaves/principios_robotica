f4a689d2f5f0a3e551328e6c67217157ed35e4b0
Guilherme Abreu Carvalho <guilhermeabreucarvalho2000@gmail.com>
2023-12-14 12:20:37 -0300

    Create sumo_wrestler.ino

---
+++ n3/sumo_wrestler/sumo_wrestler.ino
@@ -0,0 +1,17 @@
+// Sumo Wrestler Robot
+// Disciplina: Principios de Robótica      Prof.: Daniel Duarte
+// Alunos: Gerliane Chaves e Guilherme Abreu
+
+// libraries
+#include <AFMotor.h>
+#include <Ultrasonic.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
